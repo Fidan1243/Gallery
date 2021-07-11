e580bb4366d1def79e4cee0c91c7414042104482
Fidan <fidan.imanova.87@gmail.com>
2021-07-11 20:28:55 +0400

    Add project files.

---
+++ WpfApp10/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp10.Models;
+
+namespace WpfApp10
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<TheImages> ImagesG { get; set; } = new ObservableCollection<TheImages>
+        {
+            new TheImages
+            {
+                ImagePath = "Image/i1.jpg"
+            },
+            new TheImages
+            {
+                ImagePath = "Image/i2.jpg"
+            },
+            new TheImages
+            {
+                ImagePath = "Image/i3.jpg"
+            },
+            new TheImages
+            {
+                ImagePath = "Image/i4.jpg"
+            },
+            new TheImages
+            {
+                ImagePath = "Image/i5.jpg"
+            }
+        };
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            if ((sender as MenuItem).Header.ToString() == "Large")
+            {
+                this.Resources["SizePh"] = Convert.ToDouble(200);
+            }
+            else if ((sender as MenuItem).Header.ToString() == "Default")
+            {
+                this.Resources["SizePh"] = Convert.ToDouble(160);
+            }
+            else if ((sender as MenuItem).Header.ToString() == "Small")
+            {
+                this.Resources["SizePh"] = Convert.ToDouble(100);
+            }
+        }
+
+        private void mage_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var win2 = new W2();
+            win2.images = ImagesG.ToList();
+            win2.index = (sender as ListBox).SelectedIndex;
+            win2.Show();
+        }
+
+        private void mage_DragEnter(object sender, DragEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WpfApp10/W2.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfApp10.Models;
+
+namespace WpfApp10
+{
+    /// <summary>
+    /// Interaction logic for W2.xaml
+    /// </summary>
+    public partial class W2 : Window
+    {
+        public List<TheImages> images { get; set; }
+        public int index { get; set; }
+        public W2()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        private void PlayPause_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Prev_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button br)
+            {
+
+                if (br.Name == "Next")
+                {
+                    if (index + 1 < images.Count)
+                    {
+                        string a2 = images[index + 1].ImagePath;
+                        int cn = index + 1;
+                        name1.Source = new BitmapImage(new Uri($@"{a2}",UriKind.Relative));
+                        index += 1;
+                    }
+                    else
+                    {
+                        index = 0;
+                        string a2 = images[index].ImagePath;
+                        name1.Source = new BitmapImage(new Uri($@"{a2}", UriKind.Relative));
+                        
+                    }
+                }
+                else
+                {
+                    if (index <= images.Count && index > 0)
+                    {
+                        string a2 = images[index - 1].ImagePath;
+                        name1.Source = new BitmapImage(new Uri($@"{a2}", UriKind.Relative));
+                        index -= 1;
+                    }
+                    else
+                    {
+                        index = images.Count-1;
+                        string a2 = images[index].ImagePath;
+                        name1.Source = new BitmapImage(new Uri($@"{a2}", UriKind.Relative));
+
+                    }
+                }
+            }
+        }
+    }
+}
